1de6615eb0bfe8fd6f14ee66010f28423f0f7a0d
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-02-22 16:54:40 +0800

    Created models
    (Commission and CommissionType)

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class CommissionType(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField()
+
+class Commission(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    people_required=models.IntegerField()
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
+    commission_type = models.ForeignKey(CommissionType, on_delete=models.CASCADE)
