52d4bae2b5c476fe5aaf6b50f2eb623cd4af00a9
Martina <martinallamas03@gmail.com>
2026-02-26 00:03:05 +0800

    Update Book and Genre models
    add String function, fix ordering, fixed createdOn and updatedOn fields

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -5,6 +5,12 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     genre = models.ForeignKey(
@@ -15,5 +21,14 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=255)
     publicationYear = models.IntegerField()
-    createdOn = models.DateTimeField()
-    updatedOn = models.DateTimeField()
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('bookclub:book_detail', args=[str(self.id)])
+    
+    class Meta:
+        ordering = ["-publicationYear"]
