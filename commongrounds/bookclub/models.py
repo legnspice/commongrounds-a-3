d2f7b40f5e92cb16303b4b6d19b1f5d1a6d9cffb
Martina <martinallamas03@gmail.com>
2026-02-25 23:39:04 +0800

    Create Book model and update Genre Model

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -4,3 +4,15 @@ from django.urls import reverse
 class Genre(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='books'
+    )
+    author = models.CharField()
+    publicationYear = models.IntegerField()
+    createdOn = models.DateTimeField()
+    updatedOn = models.DateTimeField()
