728bf70c80750877102860f9017739a2444e0555
Martina <martinallamas03@gmail.com>
2026-02-26 00:19:43 +0800

    Add Book Club list view

---
+++ commongrounds/bookclub/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import book_list  
+
+urlpatterns = [
+    path('books/', book_list, name="books"), 
+]
+
+app_name='bookclub'
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
-
+def book_list(request):
+    books = Book.objects.all()
+    context = { "books" : books}
+    return render(request,'book_list.html',context)
