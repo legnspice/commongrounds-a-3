f685d727c943436b296bf9814bfcffc7fd6a91ce
Martina <martinallamas03@gmail.com>
2026-02-26 00:37:59 +0800

    Add book detail view

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import book_list  
+from .views import book_list, book_detail 
 
 urlpatterns = [
     path('books/', book_list, name="books"), 
+    path('book/<int:pk>/', book_detail, name='book_detail')
 ]
 
 app_name='bookclub'
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -5,3 +5,8 @@ def book_list(request):
     books = Book.objects.all()
     context = { "books" : books}
     return render(request,'book_list.html',context)
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    context = { "book" : book }
+    return render(request, 'book_detail.html', context)
