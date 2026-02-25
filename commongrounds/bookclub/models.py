558a10852138e2894764782c1aef5bc400782fd2
Martina <martinallamas03@gmail.com>
2026-02-25 23:34:55 +0800

    Create Book Model

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
