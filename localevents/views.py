2cb5d23b19546720f1934e32efd3402e3389dbe6
nammisops <immancampos@gmail.com>
2026-03-05 22:02:31 +0800

    Implement ListView and DetailView for Event model

--- localevents/views.py
+++ localevents/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Event
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'localevents/event_list.html'
+    context_object_name = 'events'
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'localevents/event_detail.html'
+    context_object_name = 'event'
