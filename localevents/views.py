c8554701455451755769f02e862da03dc134b342
nammisops <immancampos@gmail.com>
2026-03-05 23:02:54 +0800

    Fix template context bug and finalize localevents app

--- localevents/views.py
+++ localevents/views.py
@@ -6,10 +6,10 @@ from .models import Event
 class EventListView(ListView):
     model = Event
     template_name = 'localevents/event_list.html'
-    context_object_name = 'events'
+    context_object_name = 'events'  # <--- PLURAL (a list of many events)
 
 
 class EventDetailView(DetailView):
     model = Event
     template_name = 'localevents/event_detail.html'
-    context_object_name = 'event'
+    context_object_name = 'event'   # <--- SINGULAR (just one specific event)
