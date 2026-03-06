f02192097173c705a0a5f79294a326cd48689caf
Niles Cabrera <nilestristan@gmail.com>
2026-03-06 20:03:56 +0800

    fixed urls path

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import CommissionListView, CommissionDetailView
 
 urlpatterns = [
-    path('commissions/requests', CommissionListView.as_view(), name='commissionlist'),
-    path('commissions/request/<int:pk>', CommissionDetailView.as_view(), name='commissiondetail'),
+    path('requests', CommissionListView.as_view(), name='commissionlist'),
+    path('request/<int:pk>', CommissionDetailView.as_view(),
+         name='commissiondetail'),
 ]
 
 app_name = "commissions"
