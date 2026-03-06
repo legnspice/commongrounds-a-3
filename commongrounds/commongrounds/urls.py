f5ceeca108a5c119ae7360e3ffd3af116e7725e5
Niles Cabrera <nilestristan@gmail.com>
2026-03-06 21:01:04 +0800

    Revert "Merge branch 'Commission-requests-app'"
    This reverts commit ed56a3e6c0afef8c703a7ff4bbb9e2858316e3fa, reversing
    changes made to c5e777b3788a881c58f4375b67fcfc8491fb4f4a.

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
     path('localevents/', include('localevents.urls', namespace="localevents")),
     path("bookclub/", include("bookclub.urls", namespace="bookclub")),
-    path('commissions/', include('commissions.urls', namespace="commissions")),
     path('admin/', admin.site.urls),
     path('diyprojects/', include('diyprojects.urls', namespace="diyprojects")),
 ]
