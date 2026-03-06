2bd56d4168c59809c62b23f62a971a71a0d01909
Niles Cabrera <nilestristan@gmail.com>
2026-03-06 17:31:07 +0800

    fixed code inconsistencies from merge w/ localevents

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
+    path('localevents/', include('localevents.urls', namespace="localevents")),
     path('admin/', admin.site.urls),
 ]
