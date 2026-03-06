a2fed95fa5144b26020341d0ba3d65a7f065e1ac
rrahffy <rafa.jac.rjr@gmail.com>
2026-03-06 16:42:33 +0800

    Register ProjectCategory and Project in admin

--- commongrounds/diyprojects/admin.py
+++ commongrounds/diyprojects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProjectCategory, Project
 
-# Register your models here.
+
+@admin.register(ProjectCategory)
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_on', 'updated_on')
