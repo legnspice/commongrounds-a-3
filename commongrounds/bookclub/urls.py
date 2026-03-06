d08f2a88dc375e3358f90e9f1e854ae1741b7116
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-03-06 22:38:05 +0800

    Bookclub merge conflict

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import book_detail, book_list
 
 urlpatterns = [
-    path("books", book_list, name="books"),
+    path("books/", book_list, name="books"),
     path("book/<int:pk>", book_detail, name="book_detail"),
 ]
 
