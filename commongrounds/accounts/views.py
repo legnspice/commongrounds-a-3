ec520827ca3de33dbc7f5dc2f1e4aface3dfd285
Martina <martinallamas03@gmail.com>
2026-04-15 21:09:02 +0800

    Create ProfileUpdateForm

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ProfileUpdateForm
+
+@login_required
+def profile_update_view(request, username):
+    form = ProfileUpdateForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('accounts:profile_update', username=username)
+    context = {
+        'forms' : form
+    }
+    return render(request,"insert template here", context)
