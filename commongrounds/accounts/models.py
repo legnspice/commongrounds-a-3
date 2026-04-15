4cedb27f81c9a4c6185b85c18e7a8a4f8faa35ab
Martina <martinallamas03@gmail.com>
2026-04-15 20:21:04 +0800

    Update models.py
    make Profile Model

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(User):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    displayName = models.TextField(max_length=63)
+    emailAddress = models.EmailField()
+
