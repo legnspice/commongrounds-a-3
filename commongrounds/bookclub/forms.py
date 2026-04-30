3cc20f86958f2943487869d1664229a53185e6d2
Martina <martinallamas03@gmail.com>
2026-04-30 16:41:03 +0800

    Finish Book detail view skeleton

--- commongrounds/bookclub/forms.py
+++ commongrounds/bookclub/forms.py
@@ -1,3 +1,13 @@
 from django import forms
 from .models import Book, BookReview
 
+class BookReviewForm(forms.ModelForm):
+    class Meta:
+        model = BookReview
+        fields = ['title', 'comment']
+
+class BookFormFactory:
+    @classmethod
+    def get_form(cls, context):
+        if context =="review":
+            return BookReviewForm
