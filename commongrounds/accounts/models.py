abcb5593083764ed349b60e9cec11f4a36656e2a
Martina <martinallamas03@gmail.com>
2026-04-30 15:27:41 +0800

    Update book_list and book_detail view

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+    roleChoices = [
+        ("Reader", "Reader"), #REMOVE THIS LATER!!! (value - 4 database, label 4 frontend ?? )
+        ("Book Contributor", "Book Contributor")
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     displayName = models.CharField(max_length=63)
     emailAddress = models.EmailField()
+    role = models.CharField(
+        max_length=50,
+        choices=roleChoices,
+        default="Reader"
+    )
 
--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -5,6 +5,9 @@ from .views import book_detail, book_list
 urlpatterns = [
     path("books", book_list, name="books"),
     path("book/<int:pk>", book_detail, name="book_detail"),
+    # path("book/add", name="book_create"),
+    # path("book/<int:pk>/edit", name="book_edit"),
+    # path("book/<int:pk>/borrow", name="book_borrow")
 ]
 
 app_name = "bookclub"
