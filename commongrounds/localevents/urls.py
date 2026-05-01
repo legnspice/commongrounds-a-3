e393bf674cbaacb10e15c2f5a2afd65bc9a82391
nammisops <immancampos@gmail.com>
2026-05-01 22:26:03 +0800

    feat: implement EventListView with user-specific grouping and URL routing

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import EventDetailView, EventListView
+from .views import EventListView
 
 app_name = 'localevents'
 
 urlpatterns = [
-    path('events', EventListView.as_view(), name='event_list'),
-    path('event/<int:pk>', EventDetailView.as_view(), name='event_detail'),
+    path('events/', EventListView.as_view(), name='event_list'),
 ]
--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
 
 from .models import Event
 
@@ -6,10 +6,33 @@ from .models import Event
 class EventListView(ListView):
     model = Event
     template_name = 'localevents/event_list.html'
-    context_object_name = 'events'
+    context_object_name = 'all_events'
 
-
-class EventDetailView(DetailView):
-    model = Event
-    template_name = 'localevents/event_detail.html'
-    context_object_name = 'event'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        if self.request.user.is_authenticated:
+            # Retrieve the user's profile from the newly linked accounts app
+            profile = self.request.user.profile
+            
+            # 1. Events organized by the logged-in user
+            events_created = Event.objects.filter(organizer=profile)
+            
+            # 2. Events the user has registered for
+            events_signed_up = Event.objects.filter(
+                eventsignup__user_registrant=profile
+            )
+            
+            # 3. All remaining events
+            all_events = Event.objects.exclude(
+                id__in=events_created.values('id')
+            ).exclude(
+                id__in=events_signed_up.values('id')
+            )
+            
+            # Pass the groups to the template
+            context['events_created'] = events_created
+            context['events_signed_up'] = events_signed_up
+            context['all_events'] = all_events
+            
+        return context
