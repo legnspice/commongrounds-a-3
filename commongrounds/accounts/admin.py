f7f664f04f6ec5c860d858319c76b81063476e8a
nammisops <immancampos@gmail.com>
2026-05-03 23:05:27 +0800

    fix: temporary hotfix for admin.E202 in accounts app to unblock migrations

--- commongrounds/accounts/admin.py
+++ commongrounds/accounts/admin.py
@@ -1,19 +1,20 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-
 from .models import Profile, Role
-from django.contrib import admin
 
-class RoleInLine(admin.StackedInline):
-    model = Role
-    can_delete = False
+# --- COMMENTED OUT TO FIX admin.E202 ERROR ---
+# class RoleInLine(admin.StackedInline):
+#     model = Role
+#     can_delete = False
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
 class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline, RoleInLine ]
+    # Removed RoleInLine from here to unblock the system
+    inlines = [ProfileInline] 
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
