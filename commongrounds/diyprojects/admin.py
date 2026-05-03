f096b55d1896110d44672f1912166a8db46086f4
rrahffy <rafa.jac.rjr@gmail.com>
2026-05-03 22:18:35 +0800

    updated admin panel to include all diyprojects models

--- commongrounds/diyprojects/admin.py
+++ commongrounds/diyprojects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ProjectCategory, Project
+from .models import ProjectCategory, Project, Favorite, ProjectReview, ProjectRating
 
 
 @admin.register(ProjectCategory)
@@ -9,4 +9,19 @@ class ProjectCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'created_on', 'updated_on')
+    list_display = ('title', 'category', 'creator', 'created_on', 'updated_on')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('project', 'profile', 'date_favorited', 'project_status')
+
+
+@admin.register(ProjectReview)
+class ProjectReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer', 'project')
+
+
+@admin.register(ProjectRating)
+class ProjectRatingAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'project', 'score')
