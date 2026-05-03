0d897805a1364678bd79576f2f6ea63a07061a65
Martina <martinallamas03@gmail.com>
2026-05-03 21:23:19 +0800

    Add RoleAdmin

--- commongrounds/accounts/admin.py
+++ commongrounds/accounts/admin.py
@@ -1,15 +1,19 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Role
 from django.contrib import admin
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
+class RoleAdmin(admin.ModelAdmin):
+    model = Role
+
 class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline, ]
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Role, RoleAdmin)
