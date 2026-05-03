a9d4cb629fb7eb802b5d98d8c1637611c2cb2a13
rrahffy <rafa.jac.rjr@gmail.com>
2026-05-03 22:45:43 +0800

    added Project update view

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -5,6 +5,7 @@ app_name = 'diyprojects'
 
 urlpatterns = [
     path('projects', views.project_list, name='project_list'),
-    path('project/<int:pk>', views.project_detail, name='project_detail'),
     path('project/add', views.project_create, name='project_create'),
+    path('project/<int:pk>', views.project_detail, name='project_detail'),
+    path('project/<int:pk>/edit', views.project_update, name='project_update'),
 ]
--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -26,3 +26,16 @@ def project_create(request):
     else:
         form = ProjectForm()
     return render(request, 'diyprojects/project_create.html', {'form': form})
+
+
+@login_required
+def project_update(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('diyprojects:project_detail', pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'diyprojects/project_update.html', {'form': form, 'project': project})
