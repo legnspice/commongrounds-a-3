ba10e5b4d35e307c7967559cb978aa356ad7098c
rrahffy <rafa.jac.rjr@gmail.com>
2026-05-03 23:33:32 +0800

    updated project detail view to show ratings, favorites, reviews, and edit link

--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Project
-from .forms import ProjectForm
+from django.db import models
+from .models import Project, Favorite
+from .forms import ProjectForm, ProjectReviewForm, ProjectRatingForm
 
 
 def project_list(request):
@@ -25,7 +26,57 @@ def project_list(request):
 
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    return render(request, 'diyprojects/project_detail.html', {'project': project})
+    reviews = project.projectreview_set.all()
+    ratings = project.projectrating_set.all()
+    avg_rating = ratings.aggregate(models.Avg('score'))['score__avg']
+    favorites_count = project.favorite_set.count()
+    
+    review_form = None
+    rating_form = None
+    user_favorite = None
+
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        review_form = ProjectReviewForm()
+        rating_form = ProjectRatingForm()
+        user_favorite = project.favorite_set.filter(profile=profile).first()
+
+        if request.method == 'POST':
+            if 'review_submit' in request.POST:
+                review_form = ProjectReviewForm(request.POST, request.FILES)
+                if review_form.is_valid():
+                    review = review_form.save(commit=False)
+                    review.reviewer = profile
+                    review.project = project
+                    review.save()
+                    return redirect('diyprojects:project_detail', pk=pk)
+
+            elif 'rating_submit' in request.POST:
+                rating_form = ProjectRatingForm(request.POST)
+                if rating_form.is_valid():
+                    rating = rating_form.save(commit=False)
+                    rating.profile = profile
+                    rating.project = project
+                    rating.save()
+                    return redirect('diyprojects:project_detail', pk=pk)
+
+            elif 'favorite_submit' in request.POST:
+                if user_favorite:
+                    user_favorite.delete()
+                else:
+                    Favorite.objects.create(profile=profile, project=project, project_status='Backlog')
+                return redirect('diyprojects:project_detail', pk=pk)
+
+    context = {
+        'project': project,
+        'reviews': reviews,
+        'avg_rating': avg_rating,
+        'favorites_count': favorites_count,
+        'review_form': review_form,
+        'rating_form': rating_form,
+        'user_favorite': user_favorite,
+    }
+    return render(request, 'diyprojects/project_detail.html', context)
 
 
 @login_required
