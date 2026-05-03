e67ce43e8a69db707b59e4251d5308671aa0c1b5
Martina <martinallamas03@gmail.com>
2026-05-03 21:10:13 +0800

    Revert "Fix Admin Panel"
    This reverts commit f56079733a6e098d81dda844c4c8502b7cf48003.

--- commongrounds/accounts/admin.py
+++ commongrounds/accounts/admin.py
@@ -1,19 +1,15 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile, Role
+from .models import Profile
 from django.contrib import admin
-
-class RoleInLine(admin.StackedInline):
-    model = Role
-    can_delete = False
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
 class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline, RoleInLine ]
+    inlines = [ProfileInline, ]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
