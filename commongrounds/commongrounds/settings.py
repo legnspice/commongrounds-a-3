4a1412619f1be6f643094cd8a0d5c1bc2696d308
Martina <martinallamas03@gmail.com>
2026-05-04 14:59:41 +0800

    Fix Login Settings

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/bookclub/books"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
