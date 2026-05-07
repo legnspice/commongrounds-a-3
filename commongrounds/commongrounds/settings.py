23432323855706147cdc38be14137569dc53fc46
Martina <martinallamas03@gmail.com>
2026-05-07 22:12:03 +0800

    Update Settings to account for Database

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+import dj_database_url
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,10 +88,10 @@ WSGI_APPLICATION = "commongrounds.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
