d0e5f7ccc986c084dcf91043d38b02310a13cb40
Martina <martinallamas03@gmail.com>
2026-05-07 23:07:54 +0800

    Fix spelling mistake

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-LOGIN_REDIRECT_URL = "/accounts/dashbord"
+LOGIN_REDIRECT_URL = "/accounts/dashbaord"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
