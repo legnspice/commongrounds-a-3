c388c131d90fe263e93dfdd5f5d9910d7c69f083
Martina <martinallamas03@gmail.com>
2026-05-07 15:50:06 +0800

    Fix  settings

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
 LOGIN_REDIRECT_URL = "/bookclub/books"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 
--- commongrounds/merchstore/urls.py
+++ commongrounds/merchstore/urls.py
@@ -4,11 +4,11 @@ from .views import ItemListView, ItemDetailView, ProductCreateView, ProductUpdat
 urlpatterns = [
     path('items', ItemListView.as_view(), name='item_list'),
     path('item/add', ProductCreateView.as_view(), name='item_create'),
-    path('item/<int:pk>', ItemDetailView.as_view(), name='item_detail'),
-    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='item_update'),
     path('cart', CartView.as_view(), name='cart'),
     path('transactions', TransactionListView.as_view(), name='transaction_list'),
     path('complete-purchase', CompletePurchaseView.as_view(), name='complete_purchase'),
+    path('item/<int:pk>', ItemDetailView.as_view(), name='item_detail'),
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='item_update'),
 ]
 
 app_name = "merchstore"
